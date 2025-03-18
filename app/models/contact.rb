b158be516b5ca65009cc89e09998a0718fb0df4a
Bill French <wfrench@webshopmanager.com>
2025-03-18 13:54:37 -0700

    Lint

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,8 @@ class Contact < ApplicationRecord
     validates :email, presence: true, uniqueness: true
 
     def self.search(search_string)
-        name_search = FuzzyMatch.new(Contact.all, :read => :name, :threshold => Rails.application.config.fuzzy_match_threshold)
-        email_search = FuzzyMatch.new(Contact.all, :read => :email, :threshold => Rails.application.config.fuzzy_match_threshold)
+        name_search = FuzzyMatch.new(Contact.all, read: :name, threshold: Rails.application.config.fuzzy_match_threshold)
+        email_search = FuzzyMatch.new(Contact.all, read: :email, threshold: Rails.application.config.fuzzy_match_threshold)
 
         names = name_search.find_all(search_string)
         emails = email_search.find_all(search_string)
